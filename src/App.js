4dd2e8b745606032c96e1f145a95dd896ff23b91
tothricsaj <tothricsaj@gmail.com>
2020-03-28 20:14:58 +0100

    App is class2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  render() {
+    return (
     <div className="App">
       <h2>Border Radius previewer</h2>
+
+      {/* <input type="range" min="0" max="100" v/> */}
     </div>
   );
+  }
+  
 }
 
 export default App;
