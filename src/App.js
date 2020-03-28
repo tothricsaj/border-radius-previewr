0c1854d68cd36420907d623adc67c911eda80df8
tothricsaj <tothricsaj@gmail.com>
2020-03-28 21:06:03 +0100

    sliders are added

--- src/App.js
+++ src/App.js
@@ -4,15 +4,37 @@ import './App.css';
 class App extends React.Component {
 
   render() {
+
+    let sliders = () => {
+      let classes = [
+        'top-left',
+        'top-right',
+        'bottom-left',
+        'bottom-right'
+      ]
+
+      return classes.map((el, i) => {
+        return (
+          <div className={el}>
+            <input className={el + '-x'} type="range" min="1" max="100" /> 
+            <input className={el + '-y'} type="range" min="1" max="100" />
+          </div>
+        )
+        
+      })
+    }
+
     return (
     <div className="App">
       <h2>Border Radius previewer</h2>
 
-      {/* <input type="range" min="0" max="100" v/> */}
+      <div className="box"></div>
+
+      {sliders()}
+      
     </div>
   );
   }
-  
 }
 
 export default App;
